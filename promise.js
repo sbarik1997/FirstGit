249663caf4e0309396c121dfe4f86252f16de07f
supriya <supriyabarik116@gmail.com>
2023-06-22 18:10:36 +0530

    "promises all"

--- promise.js
+++ promise.js
@@ -32,24 +32,113 @@ function deletePost(){
             }else{
                 reject("ERROR");
             }
-        })
+        },2000)
     })
 };
 
-function userUpdatePost(){
-    Promise.all([createPost({title: "post1",body: "This is post 1"}),updateLastUserActivityTime()]).then(([createPostResolve,updateLastUserActivityTimeResolve]) => {
+async function userUpdatePost(){
+    return await Promise.all([createPost({title: "post1",body: "This is post 1"}),updateLastUserActivityTime()])
+};
+
+userUpdatePost().then(([createPostRes,updateLastUserActivityTimeRes]) => {
+    console.log(createPostRes.title);
+    console.log(updateLastUserActivityTimeRes);
+})
+.then(() => {
+    deletePost().then(async (msg) => {
+        console.log(msg);
+        return await Promise.all([createPost({title: "post2",body: "This is post 2"}),updateLastUserActivityTime()])
+    })
+    .then(([createPostResolve,updateLastUserActivityTimeResolve]) => {
         console.log(createPostResolve.title);
         console.log(updateLastUserActivityTimeResolve);
-    }).then(() => {
-        deletePost().then((msg) => {
-            console.log(msg);
-            Promise.all([createPost({title:"post2",body: "This is post 2"}),updateLastUserActivityTime()])
-            .then(([createPostRes,updateLastUserActivityTimeRes]) => {
-                console.log(createPostRes.title);
-                console.log(updateLastUserActivityTimeRes);
-            })
-        })
     })
-};
+})
 
-userUpdatePost();
+
+
+console.log('person1: shows tickets');
+console.log('person2: shows tickets');
+
+const promiseWifeBringingTicks = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        resolve('tickets');
+    },3000);
+});
+
+const getPopcorn = promiseWifeBringingTicks.then((t) => {
+    console.log('wife: i have tickets');
+    console.log('husband: we should go in');
+    console.log('wife: no i am hungry');
+    return new Promise((resolve,reject) => resolve(`${t} popcorn`))
+});
+
+const getButter = getPopcorn.then((t) => {
+    console.log('husband: i got some popcorn');
+    console.log('husband: we should go in');
+    console.log('wife: i need butter on my popcorn');
+    return new Promise((resolve,reject) => resolve(`${t} butter`))
+})
+
+const getColdDrinks = getButter.then((t) => {
+    console.log('husband: i got some butter on popcorn');
+    console.log('husband: anything else do you want?');
+    console.log('wife: yes i need a 7 up');
+    return new Promise((resolve,reject) => resolve(`${t} 7up`))
+})
+
+getColdDrinks.then((t) => console.log(t))
+
+console.log('person4: shows tickets');
+console.log('person5: shows tickets');
+
+
+console.log('person1: shows tickets');
+console.log('person2: shows tickets');
+
+
+const preMovie = async () => {
+    const promiseWifeBringingTicks = new Promise((resolve,reject) => {
+            setTimeout(() => {
+                resolve('tickets');
+            },3000);
+        });
+
+    const getPopcorn = new Promise((resolve,reject) => resolve('popcorn')); 
+
+    const getButter = new Promise((resolve,reject) => resolve('butter')); 
+
+    const getColdDrinks = new Promise((resolve,reject) => resolve('7up'))
+    
+    
+
+    let ticket = await promiseWifeBringingTicks;
+
+    console.log(`wife: i have ${ticket}`);
+    console.log('husband: we should go in');
+    console.log('wife: no i am hungry');
+
+    let popcorn = await getPopcorn;
+
+    console.log(`husband: i got some ${popcorn}`);
+    console.log('husband: we should go in');
+    console.log('wife: i need butter on my popcorn');
+
+    let butter = await getButter;
+
+    console.log(`husband: i got some ${butter} on popcorn`);
+    console.log('husband: anything else do you want?');
+    console.log('wife: yes i need a 7 up');
+
+    let coldDrinks = await getColdDrinks;
+
+    console.log(`husband: i bought a ${coldDrinks}`);
+    console.log('wife: lets go for the movie');
+
+    return ticket;    
+}
+
+preMovie().then((m) => console.log(`person3: shows ${m}`))
+
+console.log('person4: shows tickets');
+console.log('person5: shows tickets');
