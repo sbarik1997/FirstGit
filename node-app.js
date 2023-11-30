3a324a4d9b40f8416e5510cfd9bd4169fd133557
supriya <supriyabarik116@gmail.com>
2023-11-30 10:15:22 +0530

    create multiply arrow function and student object

--- node-app.js
+++ node-app.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+// console.log("hello world");
+
+const multiply = (a,b) => a*b;
+
+console.log(multiply(3,4));
+
+const student = {
+    name: "akash das",
+    roll: 26,
+    class: 11,
+    background: 'Arts',
+    studentDetails(){
+        console.log(`${this.name} roll no is ${this.roll} reads in class ${this.class} and stream is ${this.background}`);
+    }
+}
+
+console.log(student);
+
+student.studentDetails();
